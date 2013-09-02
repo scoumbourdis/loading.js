09d807fa5b4be272098aac8509bdf8ee2c5ae4c9
scoumbourdis <scoumbourdisj@gmail.com>
2013-09-02 05:23:41 +0100

    Add some comments for better reading of the code

--- loading.js
+++ loading.js
@@ -1,19 +1,56 @@
+/*global jQuery, console */
 var Loading;
 
-(function () {
+(function ($) {
     "use strict";
+    /**
+    * This is the description for my class.
+    *
+    * @class Loading
+    * @constructor
+    */
     Loading = function (progress_container, options) {
+        /**
+        * My property description.  Like other pieces of your comment blocks, 
+        * this can span multiple lines.
+        * 
+        * @property options
+        * @type {Object}
+        */
         this.options = options;
+        
+        /**
+        * My property description.  Like other pieces of your comment blocks, 
+        * this can span multiple lines.
+        * 
+        * @property progress_container
+        * @type {Object}
+        */
         this.progress_container = progress_container;
     };
     
+    /**
+    * My method description.  Like other pieces of your comment blocks, 
+    * this can span multiple lines.
+    *
+    * @method progress
+    * @return {Boolean} Returns true on success
+    */
     Loading.prototype.progress = function () {
         this.progress_container.find('div').show().stop().animate({
             width: this.options.percent + '%'
         }, this.options.duration);
         
+        return true;
     };
     
+    /**
+    * My method description.  Like other pieces of your comment blocks, 
+    * this can span multiple lines.
+    *
+    * @method ajaxProgress
+    * @return {Boolean} Returns true on success
+    */
     Loading.prototype.ajaxProgress = function () {
         var options = this.options,
             progress_container = this.progress_container,
@@ -38,6 +75,8 @@ var Loading;
         };
         
         $.ajax(options.ajax);
+        
+        return true;
     };
-}());
+}(jQuery));
 
